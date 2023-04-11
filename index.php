2ad61113abe49d6c3068515f21e3b771a83c5f9a
Your Name <you@example.com>
2023-04-11 16:30:17 +0200

    milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+// generatore casuale della pass
+if (isset($_GET['lunghezza'])) {
+    $lunghezza = $_GET['lunghezza'];
+    $password = generaPassword($lunghezza);
+}
+
+function generaPassword($lunghezza)
+{
+    // definisci i caratteri possibili per la password
+    $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+
+    // genera la password casuale
+    $password = '';
+    for ($i = 0; $i < $lunghezza; $i++) {
+        $password .= $caratteri[rand(0, strlen($caratteri) - 1)];
+    }
+
+    return $password;
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,7 +33,20 @@
 
 <body>
 
-    <h1>prova</h1>
+    <h1>Generatore casuale di Pass</h1>
+
+    <form method="get" action="index.php">
+        <label for="lunghezza">Dicci quanto vuoi lunga la tua nuova pass:</label>
+        <input type="number" name="lunghezza" id="lunghezza" min="1" max="50" required>
+        <input type="submit" value="Genera">
+
+        <?php
+        // stampa pass
+        echo "<p>La tua nuova password è: <strong>$password</strong></p>";
+        ?>
+    </form>
+
+
 </body>
 
 </html>
